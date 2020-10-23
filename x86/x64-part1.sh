f1739d4cae04429615d8363e861c9c94fa623328
wrtpi <71971470+wrtpi@users.noreply.github.com>
2020-10-23 23:57:23 +0800

    1
    1

---
+++ x86/x64-part1.sh
@@ -0,0 +1,16 @@
+# 添加主题/插件
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/lloyd18/luci-app-npc.git package/luci-app-npc
+#git clone https://github.com/lloyd18/npc.git package/npc
+#git clone https://github.com/lloyd18/automount.git package/automount
+
+# 解决冲突
+#sed -i 's/dnsmasq-full/dnsmasq/g' package/OpenClash/luci-app-openclash/Makefile
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+
+# 添加内核
+wget https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz&&tar -zxvf *.tar.gz
+chmod 0755 clash
+rm -rf *.tar.gz&&mkdir -p package/base-files/files/etc/openclash/core&&mv clash package/base-files/files/etc/openclash/core
